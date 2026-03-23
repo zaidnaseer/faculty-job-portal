bc14a922fe85483e2f4cd0f82584e01cc90e53fd
Zaid Naseer <myzaidnaseer@gmail.com>
2026-03-23 22:45:54 +0300

    fix: typo

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 const resumeRoutes = require("./routes/resumeRoutes");
 app.use(express.json());
-app.use(cors( {origin: process.env.FRONTEND_URL, credentials: True} ));
+app.use(cors( {origin: process.env.FRONTEND_URL, credentials: true} ));
 
 
 
